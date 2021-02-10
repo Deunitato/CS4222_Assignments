69aa9860fe813343f9768e2f5553d31eff0ca66e
Deunitato <charlotte.limwt@gmail.com>
2021-02-11 02:15:50 +0800

    Update assignment 2

--- 2/light_buzzer.c
+++ 2/light_buzzer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "contiki.h"
 #include "sys/rtimer.h"
@@ -12,22 +13,22 @@
 
 PROCESS(process_rtimer, "RTimer");
 PROCESS(process_etimer, "ETimer");
-AUTOSTART_PROCESSES(&process_rtimer, &process_etimer);
+AUTOSTART_PROCESSES(&process_rtimer);
 
-static int counter_rtimer;
 static struct rtimer timer_rtimer;
-static struct etimer timer_etimer;
 static rtimer_clock_t timeout_rtimer = RTIMER_SECOND / 4;  
 /*---------------------------------------------------------------------------*/
 static void init_opt_reading(void);
+static void buzzer(void);
 static void get_light_reading(void);
 
 /*---------------------------------------------------------------------------*/
 
 int buzzerSound = 2637;
 static int prevLUX = 0;
-static int int f = 0;
+static int f = 0;
 static int value = 0;
+static int diff = 0;
 
 
 void
@@ -35,7 +36,6 @@ do_rtimer_timeout(struct rtimer *timer, void *ptr)
 {
   /* Re-arm rtimer. Starting up the sensor takes around 125ms */
   /* rtimer period 2s */
-  clock_time_t t;
 
   rtimer_set(&timer_rtimer, RTIMER_NOW() + timeout_rtimer, 0, do_rtimer_timeout, NULL);
   get_light_reading();
@@ -50,6 +50,9 @@ get_light_reading()
   value = opt_3001_sensor.value(0);
   if(value != CC26XX_SENSOR_READING_ERROR) {
     printf("OPT: Light=%d.%02d lux\n", value / 100, value % 100);
+    diff = abs(value - prevLUX)/100;
+    printf("DIFF = %d lux ,  prevLux = %d\n\n", diff, prevLUX/100);
+    prevLUX = value;
   } else {
     printf("OPT: Light Sensor's Warming Up\n\n");
   }
@@ -67,17 +70,27 @@ init_opt_reading(void)
 static void
 buzzer()
 {
-  printf("\n\n\nBUZZER CALLED\n\n\n\n");
-  if (f == 0 && buzzer_running()) 
+  
+  if (f == 0) {
+        printf("\n\n\nBUZZER OFFED\n\n\n\n");
 	buzzer_stop(); 
-  else if (f == 1 && !buzzer_running())
+  }
+  else if (f == 1 ){
+        printf("\n\n\nBUZZER CALLED\n\n\n\n");
 	buzzer_start(buzzerSound);
+  }
 
 }
 
+//the timer is still not being used (Does not time 3mins)
+
+
 PROCESS_THREAD(process_etimer, ev, data)
 {
-  
+  static struct etimer timer_etimer;
+  //static struct timer rxtimer;
+
+
   PROCESS_BEGIN();
   buzzer_init();
   
@@ -86,22 +99,28 @@ PROCESS_THREAD(process_etimer, ev, data)
   //etimer_set(&timer_etimer, CLOCK_SECOND);  //1s timer
   
   //process_start(&process_rtimer, NULL);
-  while(1) {
-  	PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_etimer));
-	buzzer();
-        process_poll(&process_rtimer);
-        PROCESS_YIELD();
-  }
-   
-   
+  
+  etimer_set(&timer_etimer, 3 * CLOCK_SECOND);
+  printf(" Timer starting with %d \n",CLOCK_SECOND * 3);
 
+
+
+  //while(1) {
+  	PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_etimer));
+
+        buzzer();
+        //process_poll(&process_rtimer);
+        //PROCESS_YIELD();
+  //}
   PROCESS_END();
 }
 
-
+// timer not working
+//proccess not working
 
 PROCESS_THREAD(process_rtimer, ev, data)
 {
+  static struct etimer timer_etimer;
   PROCESS_BEGIN();
   init_opt_reading();
   
@@ -112,18 +131,22 @@ PROCESS_THREAD(process_rtimer, ev, data)
   while(1) {
     rtimer_set(&timer_rtimer, RTIMER_NOW() + timeout_rtimer, 0,  do_rtimer_timeout, NULL);
     //do a check
-    int diff = value/100 - prevLUX/100;
-    printf("DIFF =%d lux\n", diff);
-    prevLUX = value;
     
-    if(diff >= 300 || diff <= -300){
+    if(diff >= 200){
       if (f == 1){
          f = 0;
+	 diff = 0;
       }
       else {
          f = 1;
+         diff = 0;
       }
-      etimer_set(&timer_etimer, 4 * CLOCK_SECOND);
+      
+      //process_poll(&process_etimer);
+       process_start(&process_etimer, NULL);
+       etimer_set(&timer_etimer, 4 * CLOCK_SECOND);
+      //PROCESS_PAUSE();
+      //PROCESS_YIELD();
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_etimer));
      }
     PROCESS_YIELD();
