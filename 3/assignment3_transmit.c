7aa353a6bb09fb5f417400607142b2dd244f9fee
Deunitato <charlotte.limwt@gmail.com>
2021-03-03 19:51:54 +0800

    Add counter

--- 3/assignment3_transmit.c
+++ 3/assignment3_transmit.c
@@ -7,9 +7,9 @@
 /*---------------------------------------------------------------------------*/
 
 
-static char message[50];
+static char message[50] = {65};
 static void send(char message[], int size);
-
+static void messageInc();
 PROCESS(transmit_process, "unicasting...");
 AUTOSTART_PROCESSES(&transmit_process);
 
@@ -23,12 +23,21 @@ static void send(char message[], int size){
 
    // COMPUTE THE ADDRESS OF THE RECEIVER FROM ITS NODE ID, FOR EXAMPLE NODEID 0xBA04 MAPS TO 0xBA AND 0x04 RESPECTIVELY
    // In decimal, if node ID is 47620, this maps to 186 (higher byte) AND 4 (lower byte)
-   addr.u8[0] = // HIGH BYTE or 186 in decimal
-   addr.u8[1] = // LOW BYTE or 4 in decimal
+   addr.u8[0] = 186; // HIGH BYTE or 186 in decimal
+   addr.u8[1] = 5;// LOW BYTE or 4 in decimal
    if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
          unicast_send(&uc, &addr);
    }
 
+}
+
+static int pointer = 0;
+static void messageInc(){
+     message[pointer]+= 1;
+     if(message[pointer] > 122){
+	if(pointer ==50){pointer=0;}     	
+	else {pointer += 1;}
+     }
 }
 
 PROCESS_THREAD(transmit_process, ev, data) {
@@ -36,6 +45,8 @@ PROCESS_THREAD(transmit_process, ev, data) {
 	PROCESS_EXITHANDLER(unicast_close(&uc);)
 	PROCESS_BEGIN();
 	unicast_open(&uc, 146, &unicast_callbacks);
+	send(message, 50);
+	messageInc();
 
 
 
