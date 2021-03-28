e037a5d357510443a0446ac4594ed0177e01d8cc
Deunitato <charlotte.limwt@gmail.com>
2021-03-28 13:32:22 +0800

    Add grid implmentation

--- 4/nbr_discovery.c
+++ 4/nbr_discovery.c
@@ -12,13 +12,16 @@
 #include "powertrace.h"
 #endif
 /*---------------------------------------------------------------------------*/
-#define WAKE_TIME RTIMER_SECOND/10    // 10 HZ, 0.1s
+// duty cycle = (2 * N_SIZE - 1) / (N_SIZE * N_SIZE)
 /*---------------------------------------------------------------------------*/
-#define SLEEP_CYCLE  9        	      // 0 for never sleep
-#define SLEEP_SLOT RTIMER_SECOND/10   // sleep slot should not be too large to prevent overflow
-/*---------------------------------------------------------------------------*/
-// duty cycle = WAKE_TIME / (WAKE_TIME + SLEEP_SLOT * SLEEP_CYCLE)
-/*---------------------------------------------------------------------------*/
+#define TIME_SLOT RTIMER_SECOND/10
+#define N_SIZE 30
+
+static int row;
+static int col;
+static int currRow = 0;
+static int currCol = 0;
+/*------------------------------------0--------------------------------------*/
 // sender timer
 static struct rtimer rt;
 static struct pt pt;
@@ -36,11 +39,21 @@ broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
   leds_on(LEDS_GREEN);
   memcpy(&received_packet, packetbuf_dataptr(), sizeof(data_packet_struct));
 
-  printf("Send seq# %lu  @ %8lu  %3lu.%03lu\n", data_packet.seq, curr_timestamp, curr_timestamp / CLOCK_SECOND, ((curr_timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
+  printf("Send seq# %lu  @ %8lu  %3lu.%03lu\n", 
+    data_packet.seq, 
+    curr_timestamp, 
+    curr_timestamp / CLOCK_SECOND, 
+    ((curr_timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
 
-  printf("Received packet from node %lu with sequence number %lu and timestamp %3lu.%03lu\n", received_packet.src_id, received_packet.seq, received_packet.timestamp / CLOCK_SECOND, ((received_packet.timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
+  printf("Received packet from node %lu with sequence number %lu and timestamp %3lu.%03lu\n", 
+    received_packet.src_id, 
+    received_packet.seq, 
+    received_packet.timestamp / CLOCK_SECOND, 
+    ((received_packet.timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
+  
   leds_off(LEDS_GREEN);
 }
+
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
 /*---------------------------------------------------------------------------*/
@@ -50,33 +63,47 @@ char sender_scheduler(struct rtimer *t, void *ptr) {
   PT_BEGIN(&pt);
 
   curr_timestamp = clock_time(); 
-  printf("Start clock %lu ticks, timestamp %3lu.%03lu\n", curr_timestamp, curr_timestamp / CLOCK_SECOND, ((curr_timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
+  printf("Start clock %lu ticks, timestamp %3lu.%03lu\n", 
+    curr_timestamp, 
+    curr_timestamp / CLOCK_SECOND, 
+    ((curr_timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
 
-  while(1){
+  while(1) {
 
     // radio on
     NETSTACK_RADIO.on();
+    if (currRow == row || currCol == col) {
+      for(i = 0; i < NUM_SEND; i++) {
+        leds_on(LEDS_RED);
+        
+        data_packet.seq++;
+        curr_timestamp = clock_time();
+        data_packet.timestamp = curr_timestamp;
 
-    for(i = 0; i < NUM_SEND; i++){
-      leds_on(LEDS_RED);
-      
-      data_packet.seq++;
-      curr_timestamp = clock_time();
-      data_packet.timestamp = curr_timestamp;
+        printf("Send seq# %lu  @ %8lu ticks   %3lu.%03lu currRow: %d currCol: %d\n", 
+          data_packet.seq, 
+          curr_timestamp, 
+          curr_timestamp / CLOCK_SECOND, 
+          ((curr_timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND,
+          currRow,
+          currCol);
 
-      printf("Send seq# %lu  @ %8lu ticks   %3lu.%03lu\n", data_packet.seq, curr_timestamp, curr_timestamp / CLOCK_SECOND, ((curr_timestamp % CLOCK_SECOND)*1000) / CLOCK_SECOND);
+        packetbuf_copyfrom(&data_packet, (int)sizeof(data_packet_struct));
+        broadcast_send(&broadcast);
+        leds_off(LEDS_RED);
 
-      packetbuf_copyfrom(&data_packet, (int)sizeof(data_packet_struct));
-      broadcast_send(&broadcast);
-      leds_off(LEDS_RED);
-
-      if(i != (NUM_SEND - 1)){
-        rtimer_set(t, RTIMER_TIME(t) + WAKE_TIME, 1, (rtimer_callback_t)sender_scheduler, ptr);
-        PT_YIELD(&pt);
-      }
+        if(i != (NUM_SEND - 1)) {
+          rtimer_set(
+			t, 
+			RTIMER_TIME(t) + TIME_SLOT, 
+			1, 
+			(rtimer_callback_t)sender_scheduler, 
+			ptr);
+          PT_YIELD(&pt);
+        }
+      }  
     }
-
-    if(SLEEP_CYCLE != 0){
+    else {
       leds_on(LEDS_BLUE);
       // radio off
       NETSTACK_RADIO.off();
@@ -86,15 +113,27 @@ char sender_scheduler(struct rtimer *t, void *ptr) {
 
       // get a value that is uniformly distributed between 0 and 2*SLEEP_CYCLE
       // the average is SLEEP_CYCLE 
-      NumSleep = random_rand() % (2 * SLEEP_CYCLE + 1);  
-      printf(" Sleep for %d slots \n",NumSleep);
+      //NumSleep = random_rand() % (2 * SLEEP_CYCLE + 1);  
+      //printf(" Sleep for %d slots \n",NumSleep);
 
       // NumSleep should be a constant or static int
-      for(i = 0; i < NumSleep; i++){
-        rtimer_set(t, RTIMER_TIME(t) + SLEEP_SLOT, 1, (rtimer_callback_t)sender_scheduler, ptr);
-        PT_YIELD(&pt);
-      }
+      //for(i = 0; i < NumSleep; i++) {
+      //printf("Sleeping for %lu time\n", TIME_SLOT);
+      rtimer_set(t, RTIMER_TIME(t) + TIME_SLOT, 1, (rtimer_callback_t)sender_scheduler, ptr);
+      PT_YIELD(&pt);
+      //}
       leds_off(LEDS_BLUE);
+    }
+    if (currCol < N_SIZE - 1 && currRow < N_SIZE) {
+      currCol += 1;
+    } else {
+      if (currRow < N_SIZE - 1) {
+        currCol = 0;
+        currRow += 1;
+      } else {
+        currCol = 0;
+        currRow = 0;
+      }
     }
   }
   
@@ -109,6 +148,9 @@ PROCESS_THREAD(cc2650_nbr_discovery_process, ev, data)
 
   random_init(54222);
 
+  row = random_rand() % N_SIZE;
+  col = random_rand() % N_SIZE;
+
   #ifdef TMOTE_SKY
   powertrace_start(CLOCK_SECOND * 5);
   #endif
@@ -122,8 +164,9 @@ PROCESS_THREAD(cc2650_nbr_discovery_process, ev, data)
   #endif
 
   printf("CC2650 neighbour discovery\n");
-  printf("Node %d will be sending packet of size %d Bytes\n", node_id, (int)sizeof(data_packet_struct));
-
+  printf("Node %d will be sending packet of size %d Bytes\n", 
+    node_id, (int)sizeof(data_packet_struct));
+  printf("N_SIZE: %d row: %d col: %d\n", N_SIZE, row, col);
   // radio off
   NETSTACK_RADIO.off();
 
